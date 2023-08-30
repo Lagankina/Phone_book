bdfbdd51c47fa0a4abe971163feb402540ba0dd0
Adel <reirianet@mail.ru>
2023-08-30 11:20:54 +0300

    Домашняя работа семинар 8

--- table.py
+++ table.py
@@ -29,14 +29,21 @@ def add_new_contact(book: dict, new: list):
     book[cur_id] = new 
     
 def find_contact(book: dict, search: str):
-    result = {}
+    resault = {}
     for i, contact in book.items():
         for field in contact:
             if search.lower() in field.lower():
-                result[i] = contact
+                resault[i] = contact
                 break
-    return result
-def func_search(book: dict, cid: int):
+    return resault
+
+def  func_search(book: dict):
+    search = input("Что будем искать? ")
+    resault = find_contact(search, phone_book)
+show_contacts(resault, f"Контакт содержащий {search} не найден!" )
+
+
+def change_contact(book: dict, cid: int):
     name, phone, comment = book.get(cid)
     ch = []
     for item in ["Введите новое имя (или оставьте пустым, чтоюы не изменять): ",
@@ -46,9 +53,53 @@ def func_search(book: dict, cid: int):
     book[cid] = [ch[0] if ch[0] else name,ch[1] if ch[1] else phone, ch[2] if ch[2] else comment]
     return ch[0] if ch[0] else name
 
+def delete_contact(book: dict, cid: int):
+    name = book.pop(cid)
+    return name[0]
+
 def menu():
     menu_points = ["Открыть файл", "Сохранить файл", "Посмотреть все контакты", "Добавить контакт", "Найти контакт", "Изменить контакт", "Удалить контакт", "Выход"]
     print ("Главное меню")
     [print(f'\t{i}. {item}') for i , item in enumerate(menu_points,1)]
     choice = int(input("Выберите пункт меню: "))
     return choice
+
+while True:
+    choice = menu()
+    match choice:
+        case 1:
+            open_file(phone_book)
+            print('\nТелефонная книга успешно открыта!\n')
+        case 2:
+            save_file(phone_book)
+            print("\nТелефонная книга успешно сохранена!\n")
+        case 3:
+            show_contacts(phone_book, "Телефонная книга пцуста или не открыта!")
+        case 4:
+            new = []
+            for item in ["Введите имя: ", "Введите телефон: ", "Введите коммент: "]:
+                new.append((input(item)))
+            add_new_contact(phone_book, new)
+            print(f"\nКонтакт {new[0]} успешно добавлен!\n")
+        case 5:
+            func_search(phone_book)    
+        case 6:
+             func_search(phone_book)
+             select = int(input('Какой контакт будем изменять? '))
+             name = change_contact(phone_book, select)
+             print(f'\nКонтакт {name} успешно изменён!\n')
+        case 7:
+            func_search(phone_book)
+            select = int(input('Какой контакт будем удалять? '))
+            name = delete_contact(phone_book, select)
+            print(f'\nКонтакт {name} успешно удалён!\n')
+        case 8:
+            print('\nВсего хорошего!')
+            break
+            
+            
+            
+            
+            
+            
+            
